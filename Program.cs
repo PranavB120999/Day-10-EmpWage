f8a434fe3c5ce66fd919149539052bf4088013e1
Pranav <pranavbhople99@gmail.com>
2022-03-28 11:00:01 +0530

    Main file for Uc-4 Switch Case is added

--- Program.cs
+++ Program.cs
@@ -20,10 +20,94 @@ namespace EmployeeWages
 
         }
 
+        public static void UC2()
+        {
+            Console.WriteLine("Wlcome to UC_2");
+            int totalWorkingHour = 8;
+            int wagePerHour = 20;
+            int dailyWage = 0;
+            Random random = new Random();
+            int num = random.Next(0, 2);
+            if (num == 0)
+            {
+                Console.WriteLine("Empoloyee is Present");
+                dailyWage = totalWorkingHour * wagePerHour;
+                Console.WriteLine("Total Daily wage is: " + dailyWage);
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                Console.WriteLine("Total Daily wage is: " + dailyWage);
+            }
+
+        }
+
+        public static void UC3()
+        {
+            Console.WriteLine("Wlcome to UC_3");
+            int totalWorkingHour = 8;
+            int partTimeHour = 4;
+            int wagePerHour = 20;
+            int dailyWage = 0;
+            Random random = new Random();
+            int num = random.Next(0, 3);
+            if (num == 0)
+            {
+                Console.WriteLine("Empoloyee is Present");
+                dailyWage = totalWorkingHour * wagePerHour;
+                Console.WriteLine("Total Daily wage is: " + dailyWage);
+            }
+            else if (num == 1)
+            {
+                Console.WriteLine("Employee is doing Parttime");
+                dailyWage = partTimeHour * wagePerHour;
+                Console.WriteLine("Total Daily wage is: " + dailyWage);
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                Console.WriteLine("Total Daily wage is: " + dailyWage);
+
+            }
+        }
+
+        public static void UC4()
+        {
+            Console.WriteLine("Wlcome to UC_4");
+            int isFullTime = 1;
+            int isPartTime = 2;
+            int totalWorkingHour, wagePerHour = 20, totalWage = 0;
+
+            Random random = new Random();
+            int randomCheck = random.Next(0, 3);
+            switch (randomCheck)
+            {
+                case 1:
+                    totalWorkingHour = 8;
+                    Console.WriteLine("Employee is Present");
+                    break;
+                case 2:
+                    totalWorkingHour = 4;
+                    Console.WriteLine("Employee is doing parttime:");
+                    break;
+                default:
+                    totalWorkingHour = 0;
+                    Console.WriteLine("Employee is absent");
+                    break;
+            }
+            totalWage = wagePerHour * totalWorkingHour;
+            Console.WriteLine("Total wage is: " + totalWage);
+
+        }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to Employeewages problem ");
             Program.UC1();
+            Program.UC2();
+            Program.UC3();
+            Program.UC4();
         }
     }
 }
